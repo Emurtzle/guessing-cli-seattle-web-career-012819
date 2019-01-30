1cfe9e4a07b5749e556dea81047e13afeed8b9b4
Emily Wagner <emilywagner598@gmail.com>
2019-01-30 01:12:57 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -9,16 +9,13 @@ def run_guessing_game
 
     selection = gets.chomp
 
-    if selection == "exit"
-      break
-    end
-
     if selection.to_i == comp_guess
       puts "You guessed the correct number!"
     else
       puts "The computer guessed #{comp_guess}."
     end
   end
+  puts "Goodbye!"
 end
 
 #run_guessing_game
